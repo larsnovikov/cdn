ed2124be12f91c2efb0cf79272eae6be456f63ba
LarsNovikov <larsnovikov@yandex.ru>
2018-02-13 21:22:57 +0300

    Начало создания хранилища

---
+++ commands/CommandController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\commands;
+
+use app\models\Storage;
+use yii\console\Controller;
+use yii\helpers\FileHelper;
+
+/**
+ * Class CommandController
+ * @package app\commands
+ */
+class CommandController extends Controller
+{
+    /**
+     * @param $name
+     * @throws \yii\base\Exception
+     */
+    public function actionAddStorage($name)
+    {
+        $newPath = \Yii::$app->params['cdn']['outputPath'] . DIRECTORY_SEPARATOR . $name;
+        FileHelper::createDirectory($newPath, true);
+
+        // TODO примонтировать директорию
+
+        $storage = new Storage();
+        $storage->name = $name;
+        $storage->save();
+    }
+}
